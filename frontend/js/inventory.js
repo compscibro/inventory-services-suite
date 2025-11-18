7439571b2d978b5f71ceab6a287d90fe7d055042
Mohammed Abdur Rahman <mohammed.edu76@gmail.com>
2025-11-18 12:48:46 -0500

    added html, css, scss, js

--- frontend/js/inventory.js
+++ frontend/js/inventory.js
@@ -1,1 +1,6 @@
-print("in progress");
+const print = msg => console.log(msg);
+
+document.getElementById("insertBtn").onclick = () => print("Insert requested");
+document.getElementById("searchBtn").onclick = () => print("Search requested");
+document.getElementById("updateBtn").onclick = () => print("Update requested");
+document.getElementById("displayBtn").onclick = () => print("Display requested");
